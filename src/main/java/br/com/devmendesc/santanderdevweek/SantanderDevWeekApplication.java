16e69919ca83345a3e64e0d345351faf69625eb2
Emerson Mendes <devmendesc@gmail.com>
2023-07-25 09:52:35 -0300

    adicionando anotação de APIDeficiniton

--- src/main/java/br/com/devmendesc/santanderdevweek/SantanderDevWeekApplication.java
+++ src/main/java/br/com/devmendesc/santanderdevweek/SantanderDevWeekApplication.java
@@ -1,8 +1,11 @@
 package br.com.devmendesc.santanderdevweek;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default URL server")})
 @SpringBootApplication
 public class SantanderDevWeekApplication {
 
